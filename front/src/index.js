988fcec031278de214f3ec6882bed55c359ee702
Tolik Petras <toliketras4@gmail.com>
2023-09-04 09:39:08 +0300

    frontend was added

--- front/src/index.js
+++ front/src/index.js
@@ -6,15 +6,6 @@ import store from "./store/store";
 import App from "./App";
 import "./index.css";
 
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- front/src/socketConnection/socketConn.js
+++ front/src/socketConnection/socketConn.js
@@ -10,7 +10,7 @@ import { call, disconnect } from "../realtimeCommunication/webRTCHandler";
 let socket = null;
 
 export const connectWithSocketIOServer = () => {
-  socket = io("http://localhost:3003");
+  socket = io("https://geocall-with-react.vercel.app/");
 
   socket.on("connect", () => {
     console.log("connected to socket server");
